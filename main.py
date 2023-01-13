738b7a6b7f0cc1d5a5e59308ad62e823705a2027
Bryan Rezende <73176459+BryanFRezende@users.noreply.github.com>
2023-01-13 09:30:34 -0500

    debug 12

--- main.py
+++ main.py
@@ -43,13 +43,15 @@ Gtime = None
 quit_now = False
 
 def press(key):
-    if key == 'q':
-        print(f"'{key}'")
+    if key == "q":
+        print(f"{key}")
         quit_now = True
         stop_listening()
         return quit_now
     else:
+        quit_now = False
         stop_listening()
+        return quit_now
     stop_listening()
 
 # Loop over the main script by setting run to True and never changing it in the while loop
